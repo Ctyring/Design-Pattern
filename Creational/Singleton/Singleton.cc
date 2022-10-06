b45c9f182b55f9ecba73ccc0ccdc8b7a0e950d31
ctyring <173479693@qq.com>
2022-10-07 04:51:37 +0800

    fix bug

--- Creational/Singleton/Singleton.cc
+++ Creational/Singleton/Singleton.cc
@@ -7,16 +7,15 @@
 #include <memory>
 #include <mutex>
 using namespace std;
-class Singleton1
-{
+class Singleton1 {
     // 1. 将构造函数和析构函数私有化
     Singleton1() {}
     ~Singleton1() {}
     Singleton1(const Singleton1&) = delete;
     Singleton1& operator=(const Singleton1&) = delete;
-public:
-    static Singleton1* GetInstance()
-    {
+
+   public:
+    static Singleton1* GetInstance() {
         static Singleton1 instance;
         return &instance;
     }
@@ -26,37 +25,35 @@ public:
 // 懒汉式
 // 原理还是利用静态变量的生命周期，使用的时候再加载，节约内存
 // 不加锁线程不安全并且不好把握内存释放时间，容易内存泄漏
-class Singleton2
-{
+class Singleton2 {
     Singleton2() {}
     ~Singleton2() {}
     Singleton2(const Singleton2&) = delete;
     Singleton2& operator=(const Singleton2&) = delete;
-public:
-    static Singleton2* GetInstance()
-    {
-        if(instance == nullptr)
-        {
+
+   public:
+    static Singleton2* GetInstance() {
+        if (instance == nullptr) {
             instance = new Singleton2();
         }
         return instance;
     }
-private:
+
+   private:
     static Singleton2* instance;
 };
 
 // 智能指针
 // 懒汉式
 // 解决内存泄漏问题，但是不是线程安全的
-class Singleton3
-{
+class Singleton3 {
     Singleton3() {}
     Singleton3(const Singleton3&) = delete;
     Singleton3& operator=(const Singleton3&) = delete;
-public:
+
+   public:
     ~Singleton3() {}
-    static Singleton3* GetInstance()
-    {
+    static Singleton3* GetInstance() {
         static std::shared_ptr<Singleton3> instance(new Singleton3);
         return instance.get();
     }
@@ -65,53 +62,48 @@ public:
 // 饿汉式
 // 原理：在类加载的时候就创建好对象，不管你用不用，先创建好了再说
 // 线程安全，但是浪费内存
-class Singleton4
-{
+class Singleton4 {
     Singleton4(const Singleton4&) = delete;
     Singleton4& operator=(const Singleton4&) = delete;
-public:
-    Singleton4() {
-    }
+
+   public:
+    Singleton4() {}
     ~Singleton4() {}
-    static std::shared_ptr<Singleton4> GetInstance()
-    {     
-        return instance;
-    }
-private:
+    static std::shared_ptr<Singleton4> GetInstance() { return instance; }
+
+   private:
     static std::shared_ptr<Singleton4> instance;
 };
-std::shared_ptr<Singleton4> Singleton4::instance = std::make_shared<Singleton4>();
+std::shared_ptr<Singleton4> Singleton4::instance =
+    std::make_shared<Singleton4>();
 
 // 双重检查锁
 // 懒汉式
-class Singleton5
-{
+class Singleton5 {
     Singleton5(const Singleton5&) = delete;
     Singleton5& operator=(const Singleton5&) = delete;
-public:
-    Singleton5() {
-    }
+
+   public:
+    Singleton5() {}
     ~Singleton5() {}
-    static std::shared_ptr<Singleton5> GetInstance()
-    {
-        if(instance == nullptr)
-        {
+    static std::shared_ptr<Singleton5> GetInstance() {
+        if (instance == nullptr) {
             std::lock_guard<std::mutex> lock(mtx);
-            if(instance == nullptr)
-            {
+            if (instance == nullptr) {
                 instance = std::make_shared<Singleton5>();
             }
         }
         return instance;
     }
-private:
+
+   private:
     static std::shared_ptr<Singleton5> instance;
     static std::mutex mtx;
 };
 std::shared_ptr<Singleton5> Singleton5::instance = nullptr;
+std::mutex Singleton5::mtx;
 // 测试
-int main()
-{
+int main() {
     std::shared_ptr<Singleton5> s1 = Singleton5::GetInstance();
     std::shared_ptr<Singleton5> s2 = Singleton5::GetInstance();
     cout << s1 << endl;
